8b0b6bf612356f114a47d8e755232bec3652b6ac
Chibuokem <chibuokem_tolu@hotmail.com>
2019-09-16 12:10:56 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1> Hello, World!!!! </h1>
+      </div>
+    );
+  }
+}
+
+// eslint-disable-next-line no-undef
+export default hot(module)(App);
